b0356ed0c2c83e2ebe5f2eba0b8cab78933a40b0
Vitushan <vitushan@outlook.fr>
2025-03-24 21:07:26 +0100

    type verification

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,5 +1,10 @@
 export default class HolbertonCourse {
     constructor(name, length, students) {
-        
+        if (typeof name !== 'string') {
+            throw new TypeError('age must be a string');
+        }
+        if (typeof length !== 'number') {
+            throw new TypeError('age must be a number');
+        }
     }
 }
