140ae7c655cbb76ccb55846703dd67e06a38173b
Vitushan <vitushan@outlook.fr>
2025-03-24 22:26:19 +0100

    method

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,34 +1,38 @@
 export default class Currency {
-    constructor(code, name) {
-        if (typeof code !== 'string') {
-            throw new TypeError('Code must be a string');
-        }
-        if (typeof name !== 'string') {
-            throw new TypeError('Name must be a string');
-        }
-        this._code = code;
-        this._name = name;
+  constructor(code, name) {
+    if (typeof code !== "string") {
+      throw new TypeError("Code must be a string");
     }
+    if (typeof name !== "string") {
+      throw new TypeError("Name must be a string");
+    }
+    this._code = code;
+    this._name = name;
+  }
 
-    get code() {
-        return this._code;
-    }
+  get code() {
+    return this._code;
+  }
 
-    set code(code) {
-        if (typeof code !== 'string') {
-            throw new TypeError('Code must be a string');
-        }
-        this.code = _code;
+  set code(code) {
+    if (typeof code !== "string") {
+      throw new TypeError("Code must be a string");
     }
+    this._code = code;
+  }
 
-    get name() {
-        return this._name;
-    }
+  get name() {
+    return this._name;
+  }
 
-    set name(name) {
-        if (typeof name !== 'string') {
-            throw new TypeError('Name must be a string');
-        }
-        this.name = _name;
+  set name(name) {
+    if (typeof name !== "string") {
+      throw new TypeError("Name must be a string");
     }
+    this._name = name;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
 }
