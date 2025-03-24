5b20879282283af1efb29a175807fa5aff8bf04a
Vitushan <vitushan@outlook.fr>
2025-03-24 22:03:06 +0100

    save

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -52,7 +52,5 @@ export default class HolbertonCourse {
       throw new TypeError("Students must be an array of strings");
     }
     this._students = students;
-
-    Object.freeze(this);
   }
 }
