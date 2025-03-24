9c0a2687028a3e8af60460bc5b30d4abea5a784a
Vitushan <vitushan@outlook.fr>
2025-03-24 16:25:58 +0100

    full budget

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,6 +1,6 @@
 export default function createReportObject(employeesList) {
   return {
-    allEmployees: {...employeesList},
+    allEmployees: { ...employeesList },
 
     getNumberOfDepartements() {
       return Object.keys(employeesList).length;
