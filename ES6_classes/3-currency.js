a49c2dc2240fa03c8c1e439772ce96421c50d282
Vitushan <vitushan@outlook.fr>
2025-03-24 22:22:33 +0100

    method

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -6,7 +6,29 @@ export default class Currency {
         if (typeof name !== 'string') {
             throw new TypeError('Name must be a string');
         }
+        this._code = code;
+        this._name = name;
+    }
+
+    get code() {
+        return this._code;
+    }
+
+    set code(code) {
+        if (typeof code !== 'string') {
+            throw new TypeError('Code must be a string');
+        }
         this.code = code;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(name) {
+        if (typeof name !== 'string') {
+            throw new TypeError('Name must be a string');
+        }
         this.name = name;
     }
 }
