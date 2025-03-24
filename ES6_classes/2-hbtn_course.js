8c2758c46987b0cb31d31c05ed78a5bb9135e2f7
Vitushan <vitushan@outlook.fr>
2025-03-24 21:29:08 +0100

    type verification and stocking attribute

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -7,10 +7,43 @@ export default class HolbertonCourse {
             throw new TypeError('Length must be a number');
         }
         if  (typeof students !== 'string') {
-            throw new TypeError('students must be an array of strings');
+            throw new TypeError('students must be a strings');
         }
         this._name = name;
         this._length = length;
         this._students = students; 
     }
+}
+
+get name() {
+    return this._name;
+}
+
+set name(name) {
+    if (typeof name !== 'string') {
+        throw new TypeError('Name must be a qtring');
+    }
+    this._name = name;
+}
+
+get lenght() {
+    return this._length;
+}
+
+set length(length) {
+    if (typeof length !== 'number') {
+        throw new TypeError('Length must be a number');
+    }
+    this._length = length;
+}
+
+get students() {
+    return this._students;
+}
+
+set Students(students) {
+    if (typeof students !== 'string') {
+        throw new TypeError('Students must be a string');
+    }
+    this._students = students;
 }
