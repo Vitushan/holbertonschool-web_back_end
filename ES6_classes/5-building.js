12824cb14c049936a8287e6e72127c2919c86635
Vitushan <vitushan@outlook.fr>
2025-03-25 14:31:19 +0100

    instance

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,18 +1,5 @@
-class Building {
-  constructor(sqft) {
-    if (typeof sqft !== "number") {
-      throw new TypeError("sqft must be a number");
+export default class Building {
+    constructor(sqft) {
+
     }
-    this._sqft = sqft;
-  }
-
-  get sqft() {
-    return this._sqft;
-  }
-
-  evacuationWarningMessage() {
-    throw new Error(
-      "Class extending Building must override evacuationWarningMessage"
-    );
-  }
 }
