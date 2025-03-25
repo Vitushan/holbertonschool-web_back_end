768bc6072958f7364e811476a8903c7fe9d2aae5
Vitushan <vitushan@outlook.fr>
2025-03-25 10:03:01 +0100

    task3

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,39 @@
+import Currency from './3-currency';
+export default class Pricing {
+    constructor(amount, currency) {
+        if (typeof amount !== 'number') {
+            throw new TypeError('amount must be a number');
+        }
+        if (typeof currency !== 'Currency') {
+            throw new TypeError('currency must be a Currency');
+        }
+        this._amount = amount;
+        this._currency = currency;
+    }
+
+    get amount() {
+        return this._amount;
+    }
+
+    set amount(amount) {
+        if (typeof amount !== 'number') {
+            throw new TypeError('amount must be a number');
+        }
+        this._amount = amount;
+    }
+
+    get currency() {
+        return this._currency;
+    }
+
+    set currency(currency) {
+        if (typeof amount !== 'currency') {
+            throw new TypeError('currency must be a currency');
+        }
+        this._currency = currency;
+    }
+
+    displayFullPrice() {
+        return `${this._amount} ${this._currency}`;
+    }
+}
