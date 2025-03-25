0d751c5b09a99a203922123ddf605585ac895d81
Vitushan <vitushan@outlook.fr>
2025-03-25 15:55:22 +0100

    inheritance

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,36 +1,21 @@
-import Building from "./5-building";
+import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
-  constructor(sqft, floors)
-  super(Building) {
-    if (typeof sqft !== "number" && typeof floors !== "number") {
-      throw new TypeError("must be a number");
+  constructor(sqft, floors) {
+    super(Building);
+
+    if (typeof floors !== 'number') {
+          throw new TypeError('Floors must be a number');
+  }
     }
-    this._sqft = sqft;
     this._floors = floors;
-  }
+}
 
-  get sqft() {
-    return this._sqft;
-  }
-
-  set sqft(sqft) {
-    if (typeof sqft !== "number") {
-        throw new TypeError('must be a number');
-    }
-
-    get floors() {
+  get floors() {
         return this.floors;
-    }
-  }
+}
 
-  set floors(floors) {
-    if (typeof floors !== "number") {
-        throw new TypeError('must be a number');
-    }
-    this._floors = floors;
-  }
   evacuationWarningMessage() {
-    throw new TypeError('Evacuate slowly the NUMBER_OF_FLOORS floors');
+    return `Evacuate slowly the ${this.floors} floors`;
   }
 }
