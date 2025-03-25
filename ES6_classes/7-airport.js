aeee25e3a62d118a96794aba4855d3f0358645b6
Vitushan <vitushan@outlook.fr>
2025-03-25 16:53:06 +0100

    Class airport

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -1,24 +1,25 @@
 export default class Airport {
-    constructor(name, code) {
-        if (typeof name !== 'string') {
-            throw new TypeError('name must be a string');
-        }
-        this._name = name;
-    
-        if (typeof code !== 'string') {
-            throw new TypeError('code must be a string');
-        }
-        this._code = code;
+  constructor(name, code) {
+    if (typeof name !== "string") {
+      throw new TypeError("name must be a string");
     }
-    get name() {
-        return this._name;
-    }
+    this._name = name;
 
-    get code() {
-        return this._code;
+    if (typeof code !== "string") {
+      throw new TypeError("code must be a string");
     }
+    this._code = code;
+  }
+  get name() {
+    return this._name;
+  }
 
-    toString() {
-        return this._code;
-    }
+  get code() {
+    return this._code;
+  }
+
+  [Symbol.toStringTag] = "Airport";
+  toString() {
+    return this._code;
+  }
 }
