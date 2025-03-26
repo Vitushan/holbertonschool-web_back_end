51b2deb8e55f669ceaf32a0a361e9d55ad0492ac
Vitushan <vitushan@outlook.fr>
2025-03-26 13:18:05 +0100

    final task

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,12 +1,12 @@
 export default class Car {
   constructor(brand, motor, color) {
-    if (typeof brand !== "string") {
-      throw new TypeError("Brand must be a string");
+    if (typeof brand !== 'string') {
+      throw new TypeError('Brand must be a string');
     }
-    if (typeof motor !== "string") {
+    if (typeof motor !== 'string') {
       throw new TypeError("Motor must be a string");
     }
-    if (typeof color !== "string") {
+    if (typeof color !== 'string') {
       throw new TypeError("Color must be a string");
     }
     this._brand = brand;
