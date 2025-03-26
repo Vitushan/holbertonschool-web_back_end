74243bd35b6b020fcae25f644e3ea638236f8802
Vitushan <vitushan@outlook.fr>
2025-03-26 14:18:58 +0100

    final task

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -45,10 +45,7 @@ export default class HolbertonCourse {
   }
 
   set students(students) {
-    if (
-      !Array.isArray(students) ||
-      !students.every((student) => typeof student === "string")
-    ) {
+    if (!Array.isArray(students)) {
       throw new TypeError("Students must be an array of strings");
     }
     this._students = students;
