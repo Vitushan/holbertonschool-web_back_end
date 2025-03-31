cb9d95df93c31029fde80358ef69d44442711154
Vitushan <vitushan@outlook.fr>
2025-03-31 16:58:44 +0200

    array typed

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -14,5 +14,6 @@ export default function createInt8TypedArray(length, position, value) {
     throw new RangeError("Position outside range");
   }
 
+  view.setInt8(position, value);
   return view;
 }
