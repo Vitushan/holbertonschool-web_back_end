2168c1490467574daf1fef1a8078f1765d554269
Vitushan <vitushan@outlook.fr>
2025-03-31 13:10:54 +0200

    combined map and filter

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,13 +1,19 @@
-export default function updateStudentGradeByCity(students, location, newGrades) {
-    if (!Array.isArray(students)) {
-        return [];
-    }
+export default function updateStudentGradeByCity(
+  students,
+  location,
+  newGrades
+) {
+  if (!Array.isArray(students)) {
+    return [];
+  }
 
-    return students
-        .filter(student => student.location === location)
-        .map(student => {
-            const gradeObject = newGrades.find(grade => grade => grade.studentId === student.id);
-            const  grade = gradeObject ? gradeObject.grade : 'N/A';
-            return { ...student, grade};
-        });
+  return students
+    .filter((student) => student.location === location)
+    .map((student) => {
+      const gradeObject = newGrades.find(
+        (grade) => (grade) => grade.studentId === student.id
+      );
+      const grade = gradeObject ? gradeObject.grade : "N/A";
+      return { ...student, grade };
+    });
 }
