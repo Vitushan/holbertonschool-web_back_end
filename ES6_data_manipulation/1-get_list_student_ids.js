d7b7f4ac20b0b39ca2d43c7e8410ccf2ddfaa08e
Vitushan <vitushan@outlook.fr>
2025-03-31 10:02:29 +0200

    task 1

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,4 +1,4 @@
-import getListStudents from "./0-get_list_students"
+import getListStudents from './0-get_list_students'
 
 export default function getListStudentsIds(students) {
     if (!Array.isArray(students)) {
