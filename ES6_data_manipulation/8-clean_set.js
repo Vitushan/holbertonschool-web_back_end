492c71e24582b77e5cd2f94c64a1009fa59ad2a5
Vitushan <vitushan@outlook.fr>
2025-03-31 18:28:16 +0200

    set

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -2,4 +2,8 @@ export default function cleanSet(set, startString) {
     if (!startString || typeof startString !== 'string') {
         return;
     }
+    return [...set]
+        .filter((value) => value.startWith(startString))
+        .map((value) => value.slice(startString.length))
+        .join('-')
 }
