676a2c525090c928dae794d850cbd2e688b30cc4
Vitushan <vitushan@outlook.fr>
2025-03-31 16:52:19 +0200

    map and filter

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -7,7 +7,12 @@ export default function createInt8TypedArray(length, position, value) {
     throw new Error("All arguments must be numbers");
   }
 
-  const buffer = new ArrayBuffer(8);
-  const view = new Int8Array(buffer);
+  const buffer = new ArrayBuffer(length);
+  const view = new DataView(buffer);
+
+  if (position || length >= 0) {
+    RangeError("Position outside range");
+  }
+
+  return;
 }
-return buffer;
