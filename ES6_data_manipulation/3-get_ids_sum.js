0194122c8945576f24bcb6c795d5e691ac0ecf8b
Vitushan <vitushan@outlook.fr>
2025-03-31 12:13:16 +0200

    reduce function

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,6 +1,6 @@
 export default function getStudentIdsSum (students) {
     if (!Array.isArray(students)) {
-        return;
+        return 0;
 
     }
     return  students.reduce((accumulator, student) => accumulator + student.id, 0);
