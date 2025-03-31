fa0cdaf6cd0043290ef0e30502505c275c7c4f88
Vitushan <vitushan@outlook.fr>
2025-03-31 19:23:12 +0200

    grocerie

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,6 +1,11 @@
 export default function updateUniqueItems(groceries) {
-    if (!(groceries instanceof Map)) {
-        throw new Error('Cannot process');
-    }
+  if (!(groceries instanceof Map)) {
+    throw new Error("Cannot process");
+  }
 
+  for (var [item, quantity] of groceries) {
+    if (quantity === 1) {
+      groceries.set(item, 100);
+    }
+  }
 }
