3373260e2be8b8551fd67af50ad89a62e75dea9e
Vitushan <vitushan@outlook.fr>
2025-03-31 17:03:15 +0200

    array typed

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,17 +1,17 @@
 export default function createInt8TypedArray(length, position, value) {
   if (
-    typeof length !== 'number' ||
-    typeof position !== 'number' ||
-    typeof value !== 'number'
+    typeof length !== "number" ||
+    typeof position !== "number" ||
+    typeof value !== "number"
   ) {
-    throw new Error('All arguments must be numbers');
+    throw new Error("All arguments must be numbers");
   }
 
   const buffer = new ArrayBuffer(length);
   const view = new DataView(buffer);
 
   if (position < 0 || position >= length) {
-    throw new RangeError('Position outside range');
+    throw new RangeError("Position outside range");
   }
 
   view.setInt8(position, value);
