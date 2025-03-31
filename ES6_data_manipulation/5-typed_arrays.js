80ff9503eb76ad0d52428b85e243d5673bf6050a
Vitushan <vitushan@outlook.fr>
2025-03-31 21:39:04 +0200

    fixing task

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,17 +1,17 @@
 export default function createInt8TypedArray(length, position, value) {
   if (
-    typeof length !== "number" ||
-    typeof position !== "number" ||
-    typeof value !== "number"
+    typeof length !== 'number' ||
+    typeof position !== 'number' ||
+    typeof value !== 'number'
   ) {
-    throw new Error("All arguments must be numbers");
+    throw new Error('All arguments must be numbers');
   }
 
   const buffer = new ArrayBuffer(length);
   const view = new DataView(buffer);
 
   if (position < 0 || position >= length) {
-    throw new Error("Position outside range");
+    throw new Error('Position outside range');
   }
 
   view.setInt8(position, value);
