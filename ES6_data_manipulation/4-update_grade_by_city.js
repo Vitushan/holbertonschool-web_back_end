743f3f019f456de15bb35f6d24e22999e5854b9b
Vitushan <vitushan@outlook.fr>
2025-03-31 15:38:49 +0200

    map and filter

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -9,8 +9,8 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
       const gradeObject = newGrades.find(
         (grade) => grade.studentId === student.id
       );
-      
-      const grade = gradeObject ? gradeObject.grade : 'N/A';
+
+      const grade = gradeObject ? gradeObject.grade : "N/A";
       return { ...student, grade };
     });
 }
