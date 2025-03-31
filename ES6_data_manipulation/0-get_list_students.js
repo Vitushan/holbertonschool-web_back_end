989442ca3c15f6eb01513d4a9f820792c2d04ce9
Vitushan <vitushan@outlook.fr>
2025-03-31 08:10:35 +0200

    return arrays

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,4 +1,4 @@
-function getListStudents() {
+export default function getListStudents() {
     return [
         {id: 1, firstName: "Guillaume", location: "San Fransisco"},
         {id: 2, firstName: "James", location: "Columbia"},
