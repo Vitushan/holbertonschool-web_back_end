83a42b5fe8dbdb06b9a62ba19feae5f3c2b1e6e9
Vitushan <vitushan@outlook.fr>
2025-03-31 16:56:43 +0200

    array typed

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -10,7 +10,7 @@ export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const view = new DataView(buffer);
 
-  if (position< 0 || position >= length) {
+  if (position < 0 || position >= length) {
     throw new RangeError("Position outside range");
   }
 
