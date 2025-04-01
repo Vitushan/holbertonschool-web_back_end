e3b3047a02a848563836538a841304240599a73a
Vitushan <vitushan@outlook.fr>
2025-04-01 08:47:08 +0200

    fixing task

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -12,8 +12,8 @@ export default function createInt8TypedArray(length, position, value) {
   }
 
   const buffer = new ArrayBuffer(length);
-  const int8Array = new Int8Array(buffer); 
-  int8Array[position] = value;
+  const view = new DataView(buffer);
+  view.setInt8(position, value);
 
-  return int8Array;
+  return view;
 }
