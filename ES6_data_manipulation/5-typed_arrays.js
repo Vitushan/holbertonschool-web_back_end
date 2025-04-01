fbbbb649650c730673f1ed7f1ddbe12c1e3b8ff1
Vitushan <vitushan@outlook.fr>
2025-04-01 12:14:13 +0200

    fixing task

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -11,8 +11,9 @@ export default function createInt8TypedArray(length, position, value) {
     throw new Error('Position outside range');
   }
 
-  const array = new Int8Array(length);
-  array[position] = value;
+  const buffer = new ArrayBuffer(length);
+  const view = new DataView(buffer);
+  view.setInt8(position, value);
 
-  return array;
+  return view;
 }
