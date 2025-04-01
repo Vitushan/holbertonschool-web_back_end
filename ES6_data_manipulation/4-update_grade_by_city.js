1b1c7657ca46bd62e9f7463a157b2d23e8da2a48
Vitushan <vitushan@outlook.fr>
2025-04-01 11:12:34 +0200

    fixing task

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,4 +1,8 @@
-export default function updateStudentGradeByCity(students, location, newGrades) {
+export default function updateStudentGradeByCity(
+  students,
+  location,
+  newGrades
+) {
   if (!Array.isArray(students)) {
     return [];
   }
@@ -6,18 +10,20 @@ export default function updateStudentGradeByCity(students, location, newGrades) 
   return students
     .filter((student) => student.location === location)
     .map((student) => {
-      const gradeObject = newGrades.find((grade) => grade.studentId === student.id);
+      const gradeObject = newGrades.find(
+        (grade) => grade.studentId === student.id
+      );
 
-        let grade;
-        if (gradeObject) {
-          grade = gradeObject.grade;
-        } else {
-          grade = 'N/A';
-        }
+      let grade;
+      if (gradeObject) {
+        grade = gradeObject.grade;
+      } else {
+        grade = 'N/A';
+      }
 
       return {
         ...student,
         grade,
       };
     });
-  }
+}
