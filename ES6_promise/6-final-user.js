cf46b1d8a03053ec3443890065801c1634a1f5fd
Vitushan <vitushan@outlook.fr>
2025-04-02 11:05:00 +0200

    fixing task

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,9 +1,9 @@
 import signUpUser from "./4-user-promise";
 import uploadPhoto from "./5-photo-reject";
 
-export default function handleProfileSignup(firstName, lastName, filename) {
+export default function handleProfileSignup(firstName, lastName, fileName) {
   const promise1 = signUpUser(firstName, lastName);
-  const promise2 = uploadPhoto(filename);
+  const promise2 = uploadPhoto(fileName);
 
   return Promise.allSettled([promise1, promise2]).then((results) =>
     results.map((result) => {
@@ -14,6 +14,6 @@ export default function handleProfileSignup(firstName, lastName, filename) {
         outcome.value = result.reason;
       }
       return outcome;
-    }),
-    );
+    });
+ );
 }
