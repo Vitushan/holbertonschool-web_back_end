1f94e8d2279e21894c3b47cc580e4930a95990a1
Vitushan <vitushan@outlook.fr>
2025-04-02 15:34:08 +0200

    fixing function

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -4,7 +4,7 @@ export default function guardrail(mathFunction) {
     const result = mathFunction();
     queue.push(result);
   } catch (error) {
-    queue.push(error.message);
+    queue.push(`error: ${error.message}`);
   } finally {
     queue.push('Guardrail was processed');
   }
