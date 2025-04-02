efbf5c0e6a0fcc5c701d7d035e1c1669a4a2f001
Vitushan <vitushan@outlook.fr>
2025-04-02 11:25:37 +0200

    fixing task

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,19 +1,12 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]).then((results) => {
-    return results.map((result) => {
-      const outcome = { status: result.status };
-      if (result.status === 'fulfilled') {
-        outcome.value = result.value;
-      } else {
-        outcome.value = result.reason;
-      }
-      return outcome;
-    });
-  });
+  ]).then((results) => results.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : result.reason.toString(),
+  })));
 }
