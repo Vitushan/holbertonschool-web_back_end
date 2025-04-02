0101d14d6851b0d2518e1daaba541eb4b4f6a240
Vitushan <vitushan@outlook.fr>
2025-04-02 06:53:22 +0200

    re

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,8 +1,8 @@
-function handleResponseFromAPI(promise) {
+fexport default function handleResponseFromAPI(promise) {
   return promise
     .then(() => {
       console.log('Got a response from the API');
-      return { status: 200, body: 'Success' };
+      return { status: 200, body: 'success' };
     })
     .catch(() => {
       console.log('Got a response from the API');
