1c55cf751a74c2ab19baa2e4e091766b13386e5d
Vitushan <vitushan@outlook.fr>
2025-07-28 13:57:16 +0200

    reset

--- 9-element_length.py
+++ 9-element_length.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-
+"""
+This module provides basic arithmetic operations for floating point numbers.
+"""
 
 
 def add(a: float, b: float) -> float:
+    """
+    Add two floating point numbers and return their sum as a float.
+    """
     return a + b
