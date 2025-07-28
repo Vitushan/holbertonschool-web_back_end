efd5e8a729c2ea1d99e780d537b945d46b49d27e
Vitushan <vitushan@outlook.fr>
2025-07-28 14:10:22 +0200

    str

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
+"""
+this is a module for interpreting python3 
+"""
 
 
 def concat(str1: str, str2: str) -> str:
+    """
+    this is a func for str annotations
+    """
     return str + str2
