8c5e5a58411bbf89dc2765d5b5ec24f987773256
Vitushan <vitushan@outlook.fr>
2025-07-28 21:05:34 +0200

    random

---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""
+this module for interpreting env python3
+"""
+
+
--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
+"""
+this module for interpreting env python3
+"""
 
-import asyncio
 
-wait_random = __import__('0-basic_async_syntax').wait_random
-
-print(asyncio.run(wait_random()))
-print(asyncio.run(wait_random(5)))
-print(asyncio.run(wait_random(15)))
