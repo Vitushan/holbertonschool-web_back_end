45e70653cfd909fb1d6a273100698a3f207c82da
Vitushan <vitushan@outlook.fr>
2025-07-28 21:11:43 +0200

    random

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -7,7 +7,7 @@ this module for interpreting env python3
 import asyncio
 
 
-__import__ = ('0-basic_async_syntax').wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def  wait_n(n: int, max_delay: int) -> list[float]:
@@ -17,6 +17,4 @@ async def  wait_n(n: int, max_delay: int) -> list[float]:
     You will spawn wait_random n times with
     the specified max_delay
     """
-
-
     
