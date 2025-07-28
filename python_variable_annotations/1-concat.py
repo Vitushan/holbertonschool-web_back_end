e005545d292e4bf73ff5b541586986af4eaffe94
Vitushan <vitushan@outlook.fr>
2025-07-28 14:11:09 +0200

    str

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """
-this is a module for interpreting python3 
+This module provides string concatenation operations.
 """
 
 
 def concat(str1: str, str2: str) -> str:
     """
-    this is a func for str annotations
+    Concatenate two strings and return the result as a string.
     """
-    return str + str2
+    return str1 + str2
