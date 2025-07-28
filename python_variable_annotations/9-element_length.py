a901c3586f40ab472c1f8e5aa44f214f40fccc4d
Vitushan <vitushan@outlook.fr>
2025-07-28 14:35:28 +0200

    str

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 """
-This module provides operations to get length information for sequence elements.
+This module provides operations
+to get length information for sequence elements.
 """
 from typing import Iterable, Sequence, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """
-    Return a list of tuples containing each sequence and its length.
+    Return a list of tuples
+    containing each sequence and its length.
     """
     return [(i, len(i)) for i in lst]
