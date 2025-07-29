8329811ced11b81ed71460688aa16881a724cabc
Vitushan <vitushan@outlook.fr>
2025-07-29 13:02:14 +0200

    task4

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python3
 """
-this module for interpreting env python3
+This module provides concurrent execution of async coroutines.
 """
 
 import asyncio
 from typing import List
 
-task_wait_random = __import__('3-tasks').task_wait_random
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
-    Spawn task_wait_random n times with the specified max_delay.
+    Spawn wait_random n times with the specified max_delay.
     Return the list of all delays in ascending order without using sort().
     """
-    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    tasks = [wait_random(max_delay) for _ in range(n)]
     results = []
 
-    for task in asyncio.as_completed(tasks):
-        delay = await task
+    for coro in asyncio.as_completed(tasks):
+        delay = await coro
         results.append(delay)
 
     return results
