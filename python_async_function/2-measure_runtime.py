3c5b07f5afc4b8ca335ceaab8a811d836f384f93
Vitushan <vitushan@outlook.fr>
2025-07-29 12:08:58 +0200

    asyncio.as_completed

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -17,11 +17,10 @@ async def measure_time(n: int, max_delay: int) -> float:
     start = time.perf_counter()
     await wait_n(n, max_delay)
     end = time.perf_counter()
-    total_time = end - start
+    total_time = start - end
     try:
         delay = total_time / n
     except ZeroDivisionError:
         print("Please note that division by zero is not permitted.")
-        return 0.0
     else:
         return delay
