afa5bb9a9b188979460563e6efcda9349245961e
Vitushan <vitushan@outlook.fr>
2025-07-29 13:13:30 +0200

    preparing tasks

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -3,3 +3,12 @@
 this is a module for intepretting python3
 """
 
+import asyncio
+import random
+
+async def async_generator():
+    """
+    this is a async def generator
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
