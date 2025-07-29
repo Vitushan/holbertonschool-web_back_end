b3c462ce0448a9761d28d95a72246647f9e80abf
Vitushan <vitushan@outlook.fr>
2025-07-29 12:03:01 +0200

    asyncio.as_completed

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -17,7 +17,7 @@ async def measure_time(n: int, max_delay: int) -> float:
     start = time.perf_counter()
     await wait_n(n, max_delay)
     end = time.perf_counter()
-    total_time = start - end
+    total_time = end - start
     try:
         delay = total_time / n
     except ZeroDivisionError:
