bee18987c8eb11549cf7d8dde882a4238cd3cab1
Vitushan <vitushan@outlook.fr>
2025-07-29 11:40:01 +0200

    asyncio.as_completed

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -3,4 +3,18 @@
 this module for interpreting env python3
 """
 
+import asyncio
+import time
 
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+async def measure_time(n: int, max_delay: int) -> float:
+    """
+    Measures the total execution time of wait_n and return float
+    """
+    start = time.perf_counter()
+    await wait_n(n, max_delay)
+    end = time.perf_counter()
+    total_time = end - start
+    return total_time
