7ecd61593a0ee550fdaf3ef362017d980ca127c7
Vitushan <vitushan@outlook.fr>
2025-07-29 12:31:27 +0200

    task3

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -5,12 +5,13 @@ this module for interpreting env python3
 
 
 import asyncio
-
+from typing import Any
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> asyncio.Task:
+def task_wait_random(max_delay: int) -> asyncio.Task[Any]:
     """
-    Return an asyncio.Task for the wait_random coroutine with max_delay.
+    Return an asyncio.Task for the wait_random coroutine with max_delay
     """
-    return
+    task = wait_random(max_delay)
+    return asyncio.create_task(task)
