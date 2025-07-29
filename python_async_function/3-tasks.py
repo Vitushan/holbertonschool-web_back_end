b1cc11f720bfadef8fc1be4857bdf39ed70fe356
Vitushan <vitushan@outlook.fr>
2025-07-29 12:14:50 +0200

    task3

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -4,3 +4,13 @@ this module for interpreting env python3
 """
 
 
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """
+    Return an asyncio.Task for the wait_random coroutine with max_delay.
+    """
+    return
