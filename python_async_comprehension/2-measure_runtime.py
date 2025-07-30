67617f795249124b023d6221843c22e51b444c48
Vitushan <vitushan@outlook.fr>
2025-07-30 10:23:51 +0200

    last task

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -10,7 +10,7 @@ import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime():
+async def measure_runtime() -> float:
     """
     this is a async comprehension
     measure and return total runtime
