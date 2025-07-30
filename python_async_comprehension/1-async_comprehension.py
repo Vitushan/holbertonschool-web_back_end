ba32d2738ae38207b25ad4e1f5f3b3259d2f4731
Vitushan <vitushan@outlook.fr>
2025-07-30 10:28:05 +0200

    last task

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -5,12 +5,12 @@ this is a module for interpreting python3
 
 
 import asyncio
-
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> float:
+async def async_comprehension() -> list[float]:
     """
     this is a async comprehension that collect 10
     random numbers
