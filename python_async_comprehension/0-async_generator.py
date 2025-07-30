f59072dcdc592a51f881097f44baf858b1010768
Vitushan <vitushan@outlook.fr>
2025-07-30 09:35:57 +0200

    task 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,3 +3,17 @@
 this is a module for intepretting python3
 """
 
+
+import asyncio
+import random
+from typing import Generator
+
+
+
+async def async_generator():
+    """
+    This is a async generator function
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.randint(0, 10)
