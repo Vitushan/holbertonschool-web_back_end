80e853862a88df4f03121614abcd899ba33fdcfe
Vitushan <vitushan@outlook.fr>
2025-07-30 09:53:03 +0200

    task 0

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python3
 """
-this is a module for intepretting python3
+this is a module for interpreting python3
 """
 
 
+import asyncio
+
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension():
+    """
+    this is a async def comprehension
+    """
+    randNumbers = [randNumber async for randNumber in async_generator()]
+    return randNumbers
