46982dd2c773086dbb9e578fc6de1148cd464661
Vitushan <vitushan@outlook.fr>
2025-08-13 10:49:03 +0200

    preparing pagination tasks

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -4,10 +4,10 @@ this is a module for interpreting Python3
 """
 
 
-def  index_range(page: int, page_size: int):
+def index_range(page: int, page_size: int):
     """
     Count the start and end pagination
     """
-    start_index = (page -1) * page_size
+    start_index = (page - 1) * page_size
     end_index = page * page_size
     return start_index, end_index
