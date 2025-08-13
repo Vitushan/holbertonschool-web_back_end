e9a15bfc2799568142d68be9145e91a1986834a7
Vitushan <vitushan@outlook.fr>
2025-08-13 12:11:03 +0200

    pagination csv

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,51 +1,28 @@
-#!/usr/bin/env python3
-# This is a module for interpreting Python3
-
 import csv
-from typing import List
-
+from typing import List, Tuple
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """
-    Compute the start and end indices for pagination.
-    """
-    start_index = (page - 1) * page_size
-    end_index = page * page_size
-    return start_index, end_index
-
+    start = (page - 1) * page_size
+    end = page * page_size
+    return start, end
 
 class Server:
-    """
-    Server class to paginate a database of popular baby names.
-    """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """
-        Cached dataset.
-        Reads the dataset from the CSV file only once.
-        """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
                 dataset = [row for row in reader]
-            self.__dataset = dataset[1:]  # remove header
+            self.__dataset = dataset[1:]  # enlever la ligne d'en-tête
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """
-        Return the data for the requested page.
-        - page: starts at 1
-        - page_size: number of elements per page
-        """
-        assert isinstance(page, int) and page > 0, \
-            "page must be a positive integer"
-        assert isinstance(page_size, int) and page_size > 0, \
-            "page_size must be a positive integer"
-
+        assert isinstance(page, int) and page > 0, "page must be positive int"
+        assert isinstance(page_size, int) and page_size > 0, "page_size must be positive int"
         start, end = index_range(page, page_size)
         data = self.dataset()
         return data[start:end]
