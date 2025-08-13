33008da7f27f72d0091c629456719ab3b38de755
Vitushan <vitushan@outlook.fr>
2025-08-13 14:42:05 +0200

    pagination csv

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -1,32 +1,64 @@
 #!/usr/bin/env python3
 """
-this is a module for interpreting Python3
+Deletion-resilient hypermedia pagination
 """
 
+import csv
+from typing import List, Dict, Any
 
-from typing import Dict
 
+class Server:
+    """Server class to paginate a database of popular baby names."""
+    DATA_FILE = "Popular_Baby_Names.csv"
 
-def get_hyper_index(self, index: int = 0, page_size: int = 10) -> Dict:
-    """
-    Return page of data
-    """
-    dataset = self.indexed_dataset()
-    assert 0 <= index < len(dataset), "Index out of range"
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
 
-    data = []
-    current_index = index
-    collected = 0
+    def dataset(self) -> List[List]:
+        """Cached dataset."""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            # Remove header
+            self.__dataset = dataset[1:]
+        return self.__dataset
 
-    while collected < page_size and current_index < len(dataset):
-        if current_index in dataset:
-            data.append(dataset[current_index])
-            collected += 1
+    def indexed_dataset(self) -> Dict[int, List]:
+        """
+        Dataset indexed by starting position.
+        Deletion-resilient structure.
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            self.__indexed_dataset = {
+                i: row for i, row in enumerate(dataset)
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None,
+                        page_size: int = 10) -> Dict[str, Any]:
+        """
+        Return a deletion-resilient hypermedia pagination dict.
+        """
+        if index is None:
+            index = 0
+        assert isinstance(index, int) and index >= 0
+        assert index < len(self.dataset())
+
+        indexed_data = self.indexed_dataset()
+        data: List[List] = []
+        current_index = index
+
+        while len(data) < page_size and current_index < len(self.dataset()) + page_size:
+            if current_index in indexed_data:
+                data.append(indexed_data[current_index])
             current_index += 1
 
-    return {
-        'index': index,
-        'next_index': current_index,
-        'page_size': len(data),
-        'data': data
-    }
+        return {
+            "index": index,
+            "next_index": current_index,
+            "page_size": len(data),
+            "data": data
+        }
