66c44a133cd76b68ed5b276034a01c2fe7d6994d
Vitushan <vitushan@outlook.fr>
2025-08-13 12:09:51 +0200

    pagination csv

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -48,7 +48,5 @@ class Server:
 
         start, end = index_range(page, page_size)
         data = self.dataset()
-    
-        if start >= len(data):
-            return []
         return data[start:end]
+
