57ba52fb0f7e408ec788b3e856cfba19839c0f52
Vitushan <vitushan@outlook.fr>
2025-08-14 15:05:30 +0200

    show dbs

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-
+"""
+This is a module for interpreting python3
+"""
 
 def list_all(mongo_collection):
     """
     function list all
     """
+    if mongo_collection is None:
+        retrun []
     return list(mongo_collection.find())
