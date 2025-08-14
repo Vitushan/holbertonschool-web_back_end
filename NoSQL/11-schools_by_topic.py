e9b95cbd887edf5ef81daa49518e157fba323231
Vitushan <vitushan@outlook.fr>
2025-08-14 15:55:16 +0200

    show dbs

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -4,3 +4,20 @@ This is a module for interpreting python3
 """
 
 
+#!/usr/bin/env python3
+"""
+This module defines a function to list schools by a specific topic.
+"""
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Returns the list of school documents having a specific topic.
+
+    Args:
+        mongo_collection: The pymongo collection object
+        topic (str): The topic to search for
+
+    Returns:
+        list: List of matching school documents
+    """
+    return list(mongo_collection.find({"topics": topic}))
