4501028911998a3eea7766facc2362f674302cc9
Vitushan <vitushan@outlook.fr>
2025-08-14 15:53:59 +0200

    show dbs

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -4,3 +4,24 @@ This is a module for interpreting python3
 """
 
 
+#!/usr/bin/env python3
+"""
+This module defines a function to update the topics of a school document.
+"""
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Changes all topics of a school document based on the name.
+
+    Args:
+        mongo_collection: The pymongo collection object
+        name (str): The school name to update
+        topics (list of str): The list of topics approached in the school
+
+    Returns:
+        None
+    """
+    mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
