2f334414ed939906ab536da0dcd22290e74d0b34
Vitushan <vitushan@outlook.fr>
2025-08-18 14:22:11 +0200

    resolve problem

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (const idx of array.entries()) {
-    array[idx] = appendString + array[idx];
+  for (const [i, value] of array.entries()) {
+    array[i] = appendString + value;
   }
   return array;
 }
