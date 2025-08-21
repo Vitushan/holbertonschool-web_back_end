8b7bce5299e81f0e8ac3654a889939be03a0440c
Vitushan <vitushan@outlook.fr>
2025-08-21 11:36:42 +0200

    preparing tasks

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,6 +1,6 @@
 import Building from './5-building';
 
-class SkyHighBuilding extends Building {
+export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
     if (typeof floors !== 'number') {
@@ -17,4 +17,3 @@ class SkyHighBuilding extends Building {
     return `Evacuate slowly the ${this._floors} floors`;
   }
 }
-SkyHighBuilding();
