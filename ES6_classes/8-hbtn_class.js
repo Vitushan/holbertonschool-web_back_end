8ea41161d937c2059565571706ad85d7cb22c98a
Vitushan <vitushan@outlook.fr>
2025-08-21 14:42:54 +0200

    preparing tasks

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,13 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    if (typeof size !== 'number') {
+      throw new TypeError('size must be a number');
+    }
+    this._size = size;
+
+    if (typeof location !== 'string') {
+      throw new TypeError('location must be a string');
+    }
+    this._location = location;
+  }
+}
