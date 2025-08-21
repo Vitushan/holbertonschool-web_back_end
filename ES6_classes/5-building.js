42e7412aa354c295fae6e6da2f4e717dc097efc9
Vitushan <vitushan@outlook.fr>
2025-08-21 10:51:36 +0200

    preparing tasks

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,5 +1,8 @@
 export default class Building {
   constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft must be a number');
+    }
     this._sqft = sqft;
   }
 
