75b96bd631e765a8982aa9a8741efbfb57e79716
Vitushan <vitushan@outlook.fr>
2025-08-26 13:11:07 +0200

    Map.set

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -1,7 +1,8 @@
 export default function groceriesList() {
-  Map.set('Apples', 10);
-  Map.set('Tomatoes', 10);
-  Map.set('Pasta', 1);
-  Map.set('Rice', 1);
-  Map.set('Banana', 5);
+  const map = new Map();
+  map.set('Apples', 10);
+  map.set('Tomatoes', 10);
+  map.set('Pasta', 1);
+  map.set('Rice', 1);
+  map.set('Banana', 5);
 }
