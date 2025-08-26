0b00ef8ad68a9dd71f63df7b0adca944038accf0
Vitushan <vitushan@outlook.fr>
2025-08-26 16:51:19 +0200

    status and body

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,11 +1,9 @@
 export default function handleResponseFromAPI(promise) {
+  promise.finally(() => {
+    console.log('Got a response from the API');
+  });
+
   return promise
-    .then((response) => {
-      console.log('Got a response from the API', response);
-      return { status: 200, body: 'success' };
-    })
-    .catch(() => {
-      console.log('Got a response from the API');
-      return new Error();
-    });
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error());
 }
