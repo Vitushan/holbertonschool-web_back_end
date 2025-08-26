12a360d7455f38ac21ee04f9b84c74eb02920591
Vitushan <vitushan@outlook.fr>
2025-08-26 11:48:47 +0200

    int8array buffet

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,9 @@
+export default function createInt8TypedArray(length, position, value) {
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
+  const int8Array = new Int8Array(length);
+  int8Array[position] = value;
+
+  return new DataView(int8Array.buffer);
+}
