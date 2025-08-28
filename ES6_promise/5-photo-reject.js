d3adc43f8a5998952b4a275811ea8b4d555dbffe
Vitushan <vitushan@outlook.fr>
2025-08-28 11:43:04 +0200

    Promise resolve

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -2,5 +2,5 @@ export default function uploadPhoto(fileName) {
   if (typeof fileName !== 'string') {
     throw new TypeError('fileName must be a string');
   }
-  return Promise.reject(new Error(`${fileName}`));
+  return Promise.reject(new Error(`${fileName} cannot be processed`));
 }
