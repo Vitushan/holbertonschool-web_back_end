d681e769e36512a65c5a4235b1a0d10ab9f06b7a
Vitushan <vitushan@outlook.fr>
2025-08-29 09:07:51 +0200

    fixing error

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,17 +2,6 @@ import { signUpUser } from './4-user-promise';
 import { uploadPhoto } from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  if (typeof firstName !== 'string') {
-    throw new TypeError('firstName must be a string');
-  }
-
-  if (typeof lastName !== 'string') {
-    throw new TypeError('lastName must be a string');
-  }
-
-  if (typeof fileName !== 'string') {
-    throw new TypeError('fileName must be a string');
-  }
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
