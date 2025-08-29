3060ba643572e32062c21ea7294e3951e0077068
Vitushan <vitushan@outlook.fr>
2025-08-29 14:32:44 +0200

    try / catch

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,6 +1,13 @@
 export default function guardrail(mathFunction) {
-  queue = []
-  if (queue === 'appended') {
-    throw new Error('Guardrail was processed');
+  const queue = [];
+
+  try {
+    const result = mathFunction();
+    queue.push(result);
+  } catch (error) {
+    queue.push(`Error: ${error.message}`);
   }
+
+  queue.push('Guardrail was processed');
+  return queue;
 }
