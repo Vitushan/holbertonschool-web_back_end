03fb5853b46671546b9040ce6544e65a850aa1e4
Vitushan <vitushan@outlook.fr>
2025-09-01 13:20:17 +0200

    readline and process I/O

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,16 +1,11 @@
 #!/usr/bin/env node
 
-console.log('Welcome to Holberton School, what is your name?\n');
-
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
   const name = data.toString();
-  console.log(`Your name is: ${name}`);
+  process.stdout.write(`Your name is: ${name}\n`);
 });
 
 process.stdin.on('end', () => {
   console.log('This important software is now closing');
-});
-
-process.on('exit', (code) => {
-  console.log(`Process exited with the code ${code}`);
 });
