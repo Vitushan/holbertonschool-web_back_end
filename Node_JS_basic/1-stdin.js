017f770ad65127d94799549b988efec001b340b9
Vitushan <vitushan@outlook.fr>
2025-09-01 13:03:11 +0200

    readline and process I/O

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -11,3 +11,11 @@ rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
   console.log(`Your name is: ${name}`);
   rl.close();
 });
+
+rl.on('close', () => {
+  console.log('This important software is now closing');
+});
+
+process.on('exit', (code) => {
+  console.log(`Process is exiting with code: ${code}`);
+});
