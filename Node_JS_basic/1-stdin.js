b8610a1d4f28c6ad2fc5ed6c053059c3e19632d9
Vitushan <vitushan@outlook.fr>
2025-09-01 12:43:29 +0200

    readline and process I/O

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
+  console.log(`Your name is: ${name}`);
+  rl.close();
+});
