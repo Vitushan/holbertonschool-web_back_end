ae86ce5cc6504cdff7fe9cb35ef3b941f6dec61c
Vitushan <vitushan@outlook.fr>
2025-09-01 14:40:01 +0200

    readline and process I/O

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+
+    if (lines.length <= 1) {
+      console.log('Number of students: 0');
+      return;
+    }
+
+    const students = lines.slice(1).map((line) => line.split(','));
+
+    const fields = {};
+
+    students.forEach((student) => {
+      if (student.length < 4) return;
+      const firstName = student[0].trim();
+      const field = student[3].trim();
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+      fields[field].push(firstName);
+    });
+
+    console.log(`Number of students: ${students.length}`);
+
+    for (const field in fields) {
+      if (Object.hasOwn(fields, field)) {
+        console.log(
+          `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`
+        );
+      }
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
