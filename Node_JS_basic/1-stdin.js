8b60a9144542c2d35e15b7bb04bbfecda6544d9e
Vitushan <vitushan@outlook.fr>
2025-09-01 13:13:10 +0200

    readline and process I/O

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,21 +1,16 @@
 #!/usr/bin/env node
 
-const readline = require('readline');
+console.log('Welcome to Holberton School, what is your name?\n');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
+process.stdin.on('data', (data) => {
+  const name = data.toString();
   console.log(`Your name is: ${name}`);
-  rl.close();
 });
 
-rl.on('close', () => {
+process.stdin.on('end', () => {
   console.log('This important software is now closing');
 });
 
 process.on('exit', (code) => {
-  console.log(`Process is exiting with code: ${code}`);
+  console.log(`Process exited with the code ${code}`);
 });
