77cc17bdc641e13ab91aaf26b2c7decdc938a24f
Vitushan <vitushan@outlook.fr>
2025-09-01 14:57:42 +0200

    readline and process I/O

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -4,11 +4,22 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.setEncoding('utf-8');
 
+let inputReceived = false;
+
 process.stdin.on('data', (data) => {
+  if (inputReceived) return;
+  inputReceived = true;
+
   const name = data.toString().trim();
   process.stdout.write(`Your name is: ${name}\n`);
+
+  if (process.stdin.isTTY === false) {
+    process.stdout.write('This important software is now closing\n');
+  }
 });
 
 process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
+  if (process.stdin.isTTY) {
+    process.stdout.write('This important software is now closing\n');
+  }
 });
