8e71a2773a6b2cc0f794468f92d00dc8b272c7e1
Vitushan <vitushan@outlook.fr>
2025-09-02 10:19:14 +0200

    resolve bugg task2

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -5,7 +5,7 @@ const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-  terminal: true,
+  terminal: false,
 });
 
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
