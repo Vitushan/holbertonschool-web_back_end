04e486c3f2f64a50c3ef9f54da76bd4a3083b4c6
Vitushan <vitushan@outlook.fr>
2025-09-02 10:45:41 +0200

    resolve bugg task2

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -7,12 +7,12 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-console.log('Welcome to Holberton School, what is your name?\n');
+console.log('Welcome to Holberton School, what is your name?');
 
 rl.on('line', (name) => {
-  console.log(`Your name is: ${name}\n`);
+  console.log(`Your name is: ${name}`);
 });
 
 rl.on('close', () => {
-  console.log('This important software is now closing\n');
+  console.log('This important software is now closing');
 });
