a7cabc73c4ada3620528b45d6af0061084d52659
Vitushan <vitushan@outlook.fr>
2025-09-02 15:26:01 +0200

    prev

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,21 @@
+#!/usr/bin/env node
+
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.resume();
 
+let inputReceived = false;
+
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
+  if (name) {
+    process.stdout.write(`Your name is: ${name}\n`);
+    inputReceived = true;
+  }
 });
 
 process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
+  if (inputReceived) {
+    process.stdout.write('This important software is now closing\n');
+  }
 });
