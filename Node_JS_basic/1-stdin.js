e6790f11d0d155134c7142234fe1bc0baab74a11
Vitushan <vitushan@outlook.fr>
2025-09-02 15:29:18 +0200

    prev

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,9 +1,6 @@
 #!/usr/bin/env node
 
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-
-process.stdin.resume();
-
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   process.stdout.write(`Your name is: ${name}\n`);
