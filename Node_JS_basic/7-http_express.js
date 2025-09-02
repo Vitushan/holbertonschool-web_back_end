58e118b7fc58fbafac8fea168f36e9d4c285bdbc
Vitushan <vitushan@outlook.fr>
2025-09-02 11:24:05 +0200

    resolve bugg task2

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -3,5 +3,25 @@
 const express = require('express');
 const countStudents = require('./3-read_file_async');
 
-const databaseFile = process.argv[2];
 const app = express();
+const databaseFile = process.argv[2];
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  let response = 'This is the list of our students\n';
+  try {
+    const result = await countStudents(databaseFile);
+    response += result;
+    res.send(response);
+  } catch (err) {
+    res.send(`This is the list of our students\n'${err.message}`);
+  }
+});
+
+app.listen(1245, () => {
+  // console.log run at port 1245
+});
+module.exports = app;
