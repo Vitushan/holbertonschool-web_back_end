f5719e89ca1d2e994d9ce01235a58c3b07f93202
Vitushan <vitushan@outlook.fr>
2025-09-05 15:32:52 +0200

    utils.js

--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -1,3 +1,32 @@
-function readDatabase(path) {
+#!/usr/bin/env node
 
+const fs = require('fs').promises;
+
+async function readDatabase(path) {
+  try {
+    const data = await fs.readFile(path, 'utf8');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+    lines.shift();
+
+    const fields = {};
+
+    for (const line of lines) {
+      const parts = line.split(',');
+      if (parts.length >= 4);
+
+      const firstName = parts[0].trim();
+      const field = parts[3].trim();
+
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+      fields[field].push(firstName);
+    }
+
+    return fields;
+  } catch (err) {
+    throw new Error('the file is not accessible');
+  }
 }
+
+module.exports = readDatabase;
